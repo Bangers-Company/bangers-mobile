f121096c7560e5cd5a5b36f5aaddd079af23dcc6
Bas Voss <bas.voss@indicia.nl>
2026-03-06 13:22:19 +0100

    Removed second top bar in settings

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -92,6 +92,7 @@ export default function RootLayout() {
           name="modal"
           options={{ presentation: "modal", title: "Modal" }}
         />
+        <Stack.Screen name="settings" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
